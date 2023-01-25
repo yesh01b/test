4e6ff1123a2db32d5fcc658dae69ec69a8b84cbd
yeswanth <yeswanthb40@gmail.com>
2023-01-25 10:50:20 +0530

    Merging test

--- Yourjourneysweb/App_Start/BundleConfig.cs
+++ Yourjourneysweb/App_Start/BundleConfig.cs
@@ -14,6 +14,7 @@ namespace Yourjourneysweb
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            //Test comment 1
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
