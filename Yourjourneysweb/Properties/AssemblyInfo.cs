8348f21c39e797c8d673c34074c2e685da92fdee
yeswanth <yeswanthb40@gmail.com>
2023-01-29 11:41:13 +0530

    First commit for Pipeline testing

--- Yourjourneysweb/Properties/AssemblyInfo.cs
+++ Yourjourneysweb/Properties/AssemblyInfo.cs
@@ -5,6 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+// Test comment for pipelines
 [assembly: AssemblyTitle("Yourjourneysweb")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
